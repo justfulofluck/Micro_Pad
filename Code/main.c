cdf6c981e47232a060ca2ab5d26eba8164a0a9bc
Bhavan <bhavanbadhe@outlook.com>
2024-09-12 15:33:45 +0530

    Complete prototype code.

--- Code/main.c
+++ Code/main.c
@@ -1,74 +1,118 @@
-#include <Keypad.h>
-#include <Keyboard.h> // Corrected spelling from Keybord to Keyboard
+const uint8_t led = 13;
+const uint8_t button1 = 12;
+const uint8_t button2 = 11;
+const uint8_t button3 = 10;
+const uint8_t button4 = 8;
+const uint8_t button5 = 7;
+const uint8_t button6 = 6;
 
-const uint8_t ROWS = 2;
-const uint8_t COLS = 3;
+const uint8_t potPin = A0;
+const uint8_t ledpot = 9;
 
-// Use characters for keys instead of integers
-char keys[ROWS][COLS] = {
-  {'1', '2', '3'},
-  {'4', '5', '6'}
-};
+#define ON HIGH
+#define OFF LOW
+  
+//Funciton Prototype
+ void button_one();
+ void button_two();
+ void button_three();
+ void button_four();
+ void button_five();
+ void button_six();
+ void potmeter();
 
-uint8_t rowPins[ROWS] = {9, 8};
-uint8_t colPins[COLS] = {7, 6, 5};
-
-// Corrected the function name from makesKeymap to makeKeymap
-Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
-
-void setup() 
-{
-  Keyboard.begin(); // Corrected 'keyboard' to 'Keyboard'
+void setup() {
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+  pinMode(button1, INPUT);
+  pinMode(button2, INPUT);
+  pinMode(button3, INPUT);
+  
+  pinMode(ledpot, OUTPUT);
 }
 
-void loop() 
-{
-  char key = keypad.getKey(); // Corrected 'Keypad.getKey()' to 'keypad.getKey()'
+void loop(){
+  button_one();
+  button_two();
+  button_three();
+  button_four();
+  button_five();
+  button_six();
+  potmeter();
+}
 
-  if (key) { // Corrected 'Key' to 'key'
-    switch (key) { // Corrected 'Key' to 'key'
+// functions
 
-      // Opens Windows Explorer
-      case '1':
-        Keyboard.press(KEY_LEFT_CTRL);
-        Keyboard.press('e'); 
-        Keyboard.releaseAll();
-        break;
-      
-      // Opens Start
-      case '2':
-        Keyboard.press(KEY_LEFT_CTRL); // Corrected 'KEY_LEFT_CRTL' to 'KEY_LEFT_CTRL'
-        Keyboard.press('s');
-        Keyboard.releaseAll();
-        break;
+void potmeter() {
+  uint8_t potValue = analogRead(potPin);
+  uint8_t brightness = map(potValue, 0, 1023, 0, 255);
+  
+  analogWrite(ledpot, brightness);
+  Serial.print("Potentiometer Value: ");
+  Serial.print(potValue);
+  Serial.print(" | LED Brightness: ");
+  Serial.print(brightness);
+  
+  delay(100);
+}
 
-      // Opens File Manager
-      case '3':
-        Keyboard.press(KEY_LEFT_CTRL);
-        Keyboard.press('f');
-        Keyboard.releaseAll();
-        break;
-      
-      // Opens Task Manager
-      case '4':
-        Keyboard.press(KEY_LEFT_CTRL); // Corrected 'LEY_LEFT_CTRL' to 'KEY_LEFT_CTRL'
-        Keyboard.press(KEY_ESC); // Changed 'ESC' to 'KEY_ESC'
-        Keyboard.releaseAll();
-        break;
 
-      // Decrease brightness
-      case '5':
-        Keyboard.press(KEY_LEFT_CTRL);
-        Keyboard.press(KEY_F1); 
-        Keyboard.releaseAll();
-        break;
-      
-      // Increase brightness
-      case '6':
-        Keyboard.press(KEY_LEFT_CTRL);
-        Keyboard.press(KEY_F2); 
-        Keyboard.releaseAll();
-        break;
-    }
+void button_one(){
+  uint8_t buttonState = digitalRead(button1);
+  
+  if (buttonState == ON) {
+    digitalWrite(led, ON);
+  } else {
+    digitalWrite(led, OFF);
+  }
+}
+
+void button_two(){
+  uint8_t buttonState = digitalRead(button2);
+  
+  if (buttonState == ON) {
+    digitalWrite(led, ON);
+  } else {
+    digitalWrite(led, OFF);
+  }
+}
+
+void button_three(){
+  uint8_t buttonState = digitalRead(button3);
+  
+  if (buttonState == ON) {
+    digitalWrite(led, ON);
+  } else {
+    digitalWrite(led, OFF);
+  }
+}
+
+void button_four(){
+  uint8_t buttonState = digitalRead(button4);
+  
+  if (buttonState == ON) {
+    digitalWrite(led, ON);
+  } else {
+    digitalWrite(led, OFF);
+  }
+}
+
+void button_five(){
+  uint8_t buttonState = digitalRead(button5);
+  
+  if (buttonState == ON) {
+    digitalWrite(led, ON);
+  } else {
+    digitalWrite(led, OFF);
+  }
+}
+
+void button_six(){
+  uint8_t buttonState = digitalRead(button6);
+  
+  if (buttonState == ON) {
+    digitalWrite(led, ON);
+  } else {
+    digitalWrite(led, OFF);
   }
 }
