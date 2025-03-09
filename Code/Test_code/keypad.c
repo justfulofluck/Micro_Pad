07d9e12d64ed6717b472793092a240d2b0aec0bd
Bhavan <115586675+justfulofluck@users.noreply.github.com>
2025-03-09 15:01:22 +0530

    Update keypad.c
    Add encoder stable code.

--- Code/Test_code/keypad.c
+++ Code/Test_code/keypad.c
@@ -1,9 +1,28 @@
 #include <Keypad.h>
 
-// Encoder pins
-#define ENCODER_CLK 2
-#define ENCODER_DT 3
-#define ENCODER_BTN 4
+//Global variable
+int counter = 0;
+
+//function declaration
+void Keypad_pad();
+void encoder_one();
+void encoder_two();
+
+//Encoder 1 
+#define CLK 5
+#define DT 4
+#define SW 3
+
+int lastStateCLK = HIGH;
+int currentStateCLK;
+
+//Encoder 2
+#define CLK_two 7
+#define DT_two 6
+#define SW_two 2
+
+int lastStateCLK_two = HIGH;
+int currentStateCLK_two;
 
 // Keypad configuration
 const byte ROWS = 4;
@@ -20,61 +39,68 @@ byte colPins[COLS] = {5, 4, 3, 2};  // Connect to column pinouts
 
 Keypad customKeypad = Keypad(makeKeymap(hexKeys), rowPins, colPins, ROWS, COLS);
 
-// Encoder variables
-int lastClk = HIGH;
-bool btnPressed = false;
 
 void setup() {
-  Serial.begin(115200);  // Fixed: Only one serial.begin needed
-  pinMode(ENCODER_BTN, INPUT_PULLUP);
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(ENCODER_DT, INPUT);
-  pinMode(ENCODER_CLK, INPUT);
+  Serial.begin(115200); 
+  // Encoder 1
+  pinMode(CLK, INPUT);
+  pinMode(DT, INPUT);
+  pinMode(SW, INPUT_PULLUP);
+
+  //Encoder 2
+  pinMode(CLK_two, INPUT);
+  pinMode(DT_two, INPUT);
+  pinMode(SW_two, INPUT_PULLUP);
 }
 
 void loop() {
   // Keypad handling
+  Keypad_pad();
+  encoder_one();
+  encoder_two();
+}
+
+void Keypad_pad() {
   char customKey = customKeypad.getKey();
   if (customKey) {
     Serial.println(customKey);
   }
-
-  // Encoder handling
-  encoder();
-  encoder_button();
 }
 
-void encoder() {
-  int newClk = digitalRead(ENCODER_CLK);
-  if (newClk != lastClk) {
-    // Debounce delay
-    delay(1);
-    int dtValue = digitalRead(ENCODER_DT);
-    
-    if (newClk == LOW) {
-      if (dtValue == HIGH) {
-        Serial.println("Rotated clockwise");
-      } else {
-        Serial.println("Rotated counterclockwise");
-      }
+void encoder_one() {
+  currentStateCLK = digitalRead(CLK);
+  if (currentStateCLK != lastStateCLK) {
+    if (digitalRead(DT) != currentStateCLK) {
+      counter++;
+    } else {
+      counter--;
     }
-    lastClk = newClk;
+    Serial.println(counter);
+  }
+  lastStateCLK = currentStateCLK;
+  delay(1000);
+  if (digitalRead(SW) == LOW) {
+    Serial.println("Encoder 1 Pressed!");
+  } else {
+    Serial.println("");
   }
 }
 
-void encoder_button() {
-  if (digitalRead(ENCODER_BTN) == LOW) {
-    if (!btnPressed) {
-      btnPressed = true;
-      digitalWrite(LED_BUILTIN, HIGH);
-      Serial.println("Encoder button pressed!");
-      delay(50);  // Debounce delay
+void encoder_two() {
+  currentStateCLK_two = digitalRead(CLK_two);
+  if (currentStateCLK_two != lastStateCLK_two) {
+    if (digitalRead(DT_two) != currentStateCLK_two) {
+      counter++;
+    } else {
+      counter--;
     }
+    Serial.println(counter);
+  }
+  lastStateCLK_two = currentStateCLK_two;
+  delay(1000);
+  if (digitalRead(SW_two) == LOW) {
+    Serial.println("Encoder 2 Pressed!");
   } else {
-    if (btnPressed) {
-      btnPressed = false;
-      digitalWrite(LED_BUILTIN, LOW);
-      delay(50);  // Debounce delay
-    }
+    Serial.println("");
   }
 }
